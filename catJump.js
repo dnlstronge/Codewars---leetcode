c93d1919c8a640cff2e44712b95d3de7f4260bd7
Dan <114242578+dnlstronge@users.noreply.github.com>
2022-11-14 14:01:16 +0000

    Outline problem

---
+++ catJump.js
@@ -0,0 +1,15 @@
+/*Description
+
+An infinite number of shelves are arranged one above the other in a staggered fashion.
+The cat can jump up to 3 shelves at the same time: from shelf 1 to shelf 2 or 4 (the cat cannot climb on the shelf directly above its head), according to the illustration:
+
+Input
+Start and finish shelf numbers (always positive integers, finish no smaller than start)
+
+Task
+Find the minimum number of jumps to go from start to finish
+
+Example
+Start 1, finish 5, then answer is 2 (1 => 4 => 5 or 1 => 2 => 5)
+
+*/
