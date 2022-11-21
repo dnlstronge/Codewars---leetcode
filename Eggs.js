b83c4e17603cc394cdee8deeb29f373839a8eb68
Dan <114242578+dnlstronge@users.noreply.github.com>
2022-11-21 12:31:01 +0000

    Create Eggs.js

---
+++ Eggs.js
@@ -0,0 +1,13 @@
+/* 
+
+Unscramble the eggs.
+
+The string given to your function has had an "egg" inserted directly after each consonant. You need to return the string before it became eggcoded.
+
+Example
+unscrambleEggs("Beggegeggineggneggeregg"); => "Beginner"
+//             "B---eg---in---n---er---"
+
+*/
+
+unscrambleEggs()
