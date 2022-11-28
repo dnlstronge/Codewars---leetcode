a65928574bf124576c65f84073ef5e5de2ab8ca4
Dan <114242578+dnlstronge@users.noreply.github.com>
2022-11-28 14:32:15 +0000

    Logic works, need conditionals?

--- hashtagGen.js
+++ hashtagGen.js
@@ -10,19 +10,16 @@ If the final result is longer than 140 chars it must return false.
 If the input or the result is an empty string it must return false.
 
 */
-                              
-function iterate(string){
- let answer = string
-    .split(' ')
-    .map(l => l.charAt(0).toUpperCase() + l.slice(1))
-console.log(answer)
+
+function generateHashTag(str){
+    let answer = str.split(' ').map(l => l.charAt(0).toUpperCase() + l.slice(1))
+return `#${answer.toString().replace(/,/g, '')}`
 }
 
-
-
-iterate('fancy going to the shop')
   //let answer = array.toString().replace(/,/g, '')
 
 // sofar I can return the string, but need to capitalise each char at 0?
 // think maybe return a mapped array, change char at instead of for loop.
 // now I hav capitlised the letters I need to join? then conc '#'
+// okay - now I think thats the logic sorted, need conditionals? 
+// must return false in two cases
