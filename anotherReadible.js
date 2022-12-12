7e1392aad4da749e52f59ecad8eade74abfa851e
Dan <114242578+dnlstronge@users.noreply.github.com>
2022-12-12 16:54:29 +0000

    Update anotherReadible.js

--- anotherReadible.js
+++ anotherReadible.js
@@ -1,3 +1,8 @@
+
+//// I think (!) I have go it... need to test
+
+
+
 function formatDuration (seconds) {
  
  const yS = 36536000
@@ -8,29 +13,70 @@ function formatDuration (seconds) {
 // years
 
   let years = Math.floor(seconds / yS) 
-  console.log(years)
+  //console.log(years)
 
   let days = Math.floor( seconds % yS / dS)
-  console.log(days)
+  //console.log(days)
 
   let hours = Math.floor( seconds % yS % dS / hrS)
-  console.log(hours)
+  //console.log(hours)
   
   let minutes = Math.floor( seconds % yS % dS % hrS / minS)
-  console.log(minutes)
+  //console.log(minutes)
 
   let secs = Math.floor ( seconds % yS % dS % hrS % minS)
-  console.log(secs)
+  //console.log(secs)
 
 let array1 = []
 let array2 = []
 
-if (years > 1 ) { array1.push(`${years} years`)}
+  if (years === 1 ) { array1.push(`${years} year`)}
+  if (years > 1 ) { array1.push(`${years} years`)}
+
+  if (days === 1 ) { array1.push(`${days} day`)}
+  if (days > 1 ) { array1.push(`${days} days`)}
+
+  if ( hours === 1 ) { array1.push(`${hours} hour`)}
+  if (hours > 1 ) { array1.push(`${hours} hours`)}
+
+  if (minutes === 1 ) { array1.push(`${minutes} minute`)}
+  if (minutes > 1 ) { array1.push(`${minutes} minutes`)}
+
+  if (secs === 1 ) { array1.push(`${secs} second`)}
+  if (secs > 1 ) { array1.push(`${secs} seconds`)}
+
+
+let solution = ''
+switch (array1.length) {
+  case 0:
+  console.log(array1[0])
+
+  case 1: 
+  solution = `${array[0]}`
+  
+  case 2:
+  solution = `${array1[0]} and ${array1[1]}`
+  break;
+ 
+  case 3: 
+   solution = `${array1[0]}, ${array1[1]} and ${array1[2]}`
+   break;
+ 
+  case 4: 
+  solution = `${array1[0]},${array1[1]}, ${array1[2]} and ${array1[3]}`
+  break;
+  
+  case 5: 
+  solution = `${array1[0]}, ${array1[1]}, ${array1[2]}, ${array1[3]} and ${array1[4]}`
+  break;
+
+default: 
+console.log('error')
+}
+console.log(solution)
 
 
 
 }
 
-
-// maybe user arrays? to determine if an array has a certain length it can be separated with the appropriate punctuation,
-// have condition decide what gets pushed 'plural; or 'singular
+formatDuration(3667)
