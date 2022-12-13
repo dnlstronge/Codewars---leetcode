9a989d18d06cd8b7e1cb2c7570033b9a2f4004c4
Dan <114242578+dnlstronge@users.noreply.github.com>
2022-12-13 10:51:14 +0000

    Solved (refactor)
    Kata (4kyu)
    
    DESCRIPTION:
    Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
    
    The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
    
    It is much easier to understand with an example:
    
    * For seconds = 62, your function should return 
        "1 minute and 2 seconds"
    * For seconds = 3662, your function should return
        "1 hour, 1 minute and 2 seconds"
    
    For the purpose of this Kata, a year is 365 days and a day is 24 hours.
    
    Note that spaces are important.
    
    Detailed rules
    The resulting expression is made of components like 4 seconds, 1 year, etc. In general, a positive integer and one of the valid units of time, separated by a space. The unit of time is used in plural if the integer is greater than 1.
    
    The components are separated by a comma and a space (", "). Except the last component, which is separated by " and ", just like it would be written in English.
    
    A more significant units of time will occur before than a least significant one. Therefore, 1 second and 1 year is not correct, but 1 year and 1 second is.
    
    Different components have different unit of times. So there is not repeated units like in 5 seconds and 1 second.
    
    A component will not appear at all if its value happens to be zero. Hence, 1 minute and 0 seconds is not valid, but it should be just 1 minute.
    
    A unit of time must be used "as much as possible". It means that the function should not return 61 seconds, but 1 minute and 1 second instead. Formally, the duration specified by of a component must not be greater than any valid more significant unit of time.

--- anotherReadible.js
+++ anotherReadible.js
@@ -5,61 +5,43 @@ function formatDuration (seconds) {
  const hrS =  3600
  const minS = 60
 
+ let array1 = []
+ let solution = ''
 // years
 
   let years = Math.floor(seconds / yS) 
   //console.log(years)
+    if (years === 1 ) { array1.push(`${years} year`)}
+    if (years > 1 ) { array1.push(`${years} years`)}
   
 // days
   let days = Math.floor( seconds % yS / dS)
   //console.log(days)
+    if (days === 1 ) { array1.push(`${days} day`)}
+    if (days > 1 ) { array1.push(`${days} days`)}
   
 // hours
   let hours = Math.floor( seconds % yS % dS / hrS)
   //console.log(hours)
+    if ( hours === 1 ) { array1.push(`${hours} hour`)}
+    if (hours > 1 ) { array1.push(`${hours} hours`)}
+
   
 // minutes  
   let minutes = Math.floor( seconds % yS % dS % hrS / minS)
   //console.log(minutes)
+    if (minutes === 1 ) { array1.push(`${minutes} minute`)}
+    if (minutes > 1 ) { array1.push(`${minutes} minutes`)}
+
 
 // seconds
   let secs = Math.floor ( seconds % yS % dS % hrS % minS)
   //console.log(secs)
-
-let array1 = []
-
-// years
-
-
-  if (years === 1 ) { array1.push(`${years} year`)}
-  if (years > 1 ) { array1.push(`${years} years`)}
+   if (secs === 1 ) { array1.push(`${secs} second`)}
+   if (secs > 1 ) { array1.push(`${secs} seconds`)}
  
-// days 
- 
-  if (days === 1 ) { array1.push(`${days} day`)}
-  if (days > 1 ) { array1.push(`${days} days`)}
 
- // hours
- 
-  if ( hours === 1 ) { array1.push(`${hours} hour`)}
-  if (hours > 1 ) { array1.push(`${hours} hours`)}
-
- // minutes
- 
-  if (minutes === 1 ) { array1.push(`${minutes} minute`)}
-  if (minutes > 1 ) { array1.push(`${minutes} minutes`)}
-
- // seconds
- 
-  if (secs === 1 ) { array1.push(`${secs} second`)}
-  if (secs > 1 ) { array1.push(`${secs} seconds`)}
-
-
-let solution = ''
 switch (array1.length) {
-  /*case 1 :
-  if(array1[0] === 0) {solution = 'now'}
-  break; */
 
   case 1: 
   solution = `${array1[0]}`
@@ -89,7 +71,3 @@ return 'now'
   return solution
 }
 
-
-
-
-formatDuration(132030240)
