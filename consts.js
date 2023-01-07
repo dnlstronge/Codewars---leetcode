de342c00b56b92798509897ceaaef6c87aa28306
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-01-07 14:26:49 +0000

    Update consts.js

--- consts.js
+++ consts.js
@@ -1,16 +1,8 @@
 function consonantCount(str) {
-  let solution = 0
-  for (let i=0; i < str.length; i++) {
-    if(str[i] !== 'a' && str[i]!== 'e' && str[i] !== 'i' && str[i] !== 'o' && str[i] !== 'u' && str[i]!==' ' ) {
-      solution++
-    
+  let solution = str.match(/(?![aeiou])[a-z]/gi, "").length
+
+
+console.log(solution)
+return solution
     }
   
-  }
-  return solution;
-console.log(solution)
-}
-
-consonantCount('hello world')
-
-/// altered to account for empty space throwing additional cons++
