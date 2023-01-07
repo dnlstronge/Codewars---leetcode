cb94c4ce0224109e962b9457541e5d04305bcf13
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-01-07 13:55:27 +0000

    Update consts.js

--- consts.js
+++ consts.js
@@ -1,5 +1,14 @@
 /// find the number of consonants in a given sentene: 
 
 function consonantCount(str) {
-  // ...
+  let solution = 0
+  for (let i=0; i < str.length; i++) {
+    if(str[i] !== 'a' && str[i]!== 'e' && str[i] !== 'i' && str[i] !== 'o' && str[i] !== 'u') {
+      solution++
+    
+    }
+  
+  }
+  return solution
+console.log(solution)
 }
