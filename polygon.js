5bd3c48344e4490e51f22bb178ab63268b6ec061
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-01-16 15:47:59 +0000

    solved

--- polygon.js
+++ polygon.js
@@ -1,3 +1,8 @@
 /* Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2. */
 
-//   
+//   the sum of internal angles is equal to 2N - 4 * 90 degrees so:
+
+function angle(n) {
+ return ((2*n) - 4) * 90
+}
+
