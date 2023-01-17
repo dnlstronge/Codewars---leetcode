f0c2d58670367a9f47a8b4f2d90fde77fa5930c5
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-01-17 15:22:13 +0000

    Update oddevens.js

--- oddevens.js
+++ oddevens.js
@@ -1,1 +1,12 @@
 /* Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
+
+// an even number ends in 0 2 4 6 or 8 so with that in mind.....
+
+function evenOrOdd(n) {
+ const sol = n.toString().split('')
+ console.log(sol)
+
+sol[sol.length-1] === 0 ? console.log('even') : console.log('odd')
+}
+
+evenOrOdd(44)
