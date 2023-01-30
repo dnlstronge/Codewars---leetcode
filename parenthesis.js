24ce01f5676f847598fb4d560179e3fcaf036264
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-01-30 14:51:10 +0000

    solved

--- parenthesis.js
+++ parenthesis.js
@@ -15,3 +15,15 @@ for (const char of s) {
   return stack.length === 0;
 };
  
+/* Or this I like.........*/
+
+function validParentheses(parens){
+  var n = 0;
+  for (var i = 0; i < parens.length; i++) {
+    if (parens[i] == '(') n++;
+    if (parens[i] == ')') n--;
+    if (n < 0) return false;
+  }
+  
+  return n == 0;
+}
