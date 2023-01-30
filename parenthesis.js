16966f8eca089e24d699f85106d0f24cd2f8b014
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-01-30 14:50:32 +0000

    Update parenthesis.js

--- parenthesis.js
+++ parenthesis.js
@@ -1,31 +1,17 @@
-/*
+/* think this is right */
 
-Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
-
-Examples
-"()"              =>  true
-")(()))"          =>  false
-"("               =>  false
-"(())((()())())"  =>  true
-
-
-Constraints
-0 <= input.length <= 100*/
-
-function validParentheses(parens) {
-let a = []
-
-
-for (let i= 0; i < parens.length; i++)
- if (parens[i] === "(") {
-   a.push("(") 
-if (parens[i] === ")") {
-  a.pop()
-}
-
-console.log(a.length)
-
-}
-}
-validParentheses("(())()())))((())))))))))))))))))))")
-
+var isValid = function(s) {
+  const stack = [];
+  const characters = { ')': '('};
+for (const char of s) {
+      
+    if (!characters[char]){
+       stack.push(char); 
+    } 
+    else if (stack.pop() !== characters[char]){
+        return false;
+    } 
+  }
+  return stack.length === 0;
+};
+ 
