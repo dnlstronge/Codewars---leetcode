97cb7c11817dd73927d60f89c69ca01c1de88c82
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-01-30 16:36:14 +0000

    solved

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -10,7 +10,22 @@ answer[i] == "Buzz" if i is divisible by 5.
 answer[i] == i (as a string) if none of the above conditions are true.  */
 
 
+const fizzbuzz = (n) => {
+  let array=[]
 
+for (let i = 1; i <= n; i ++) {
+  if(i%5 == 0 && i%3  == 0) {array.push("FizzBuzz")}
+  else if (i%3 == 0) {array.push("Fizz")}
+  else if (i%5 == 0) {array.push("Buzz")}
+  else {
+ array.push(i)}
+
+} 
+console.log(array)
+return array
+}
+
+fizzbuzz(30)
 
 
 
