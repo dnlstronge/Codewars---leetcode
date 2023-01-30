683ba6bbcfde2073c0d5b67395132764d7be48fb
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-01-30 13:26:26 +0000

    Update parenthesis.js

--- parenthesis.js
+++ parenthesis.js
@@ -11,3 +11,14 @@ Examples
 
 Constraints
 0 <= input.length <= 100*/
+
+
+// check one: odd vs even? 
+
+function validParentheses(parens) {
+  
+let a = parens.split("", ).filter((char) => char === "(").length
+let b = parens.split("", ).filter((char) => char === ")").length
+console.log(a, b)
+}
+validParentheses("(())")
