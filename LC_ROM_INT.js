1df57e7b41b2acf4991f124088253bc2d7a6c5cd
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-02-21 11:24:05 +0000

    Update LC_ROM_INT.js

--- LC_ROM_INT.js
+++ LC_ROM_INT.js
@@ -8,8 +8,33 @@ Constraints:
 s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
 It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 
-
+  I = 1,
+    V = 5,
+    X = 10,
+    L = 50,
+    C = 100,
+    D = 500,
+    M = 1000,
 
 */
+
+const romanInt = (roman) => {
+  
+  const solution = 0
+  const numerals = {
+    I : "I",
+    V : "V",
+    X : "X",
+    L : "L",
+    C : "C",
+    D : "D",
+    M : "M",
+  }
+    for (let i = 0; i = roman.length, i ++ )
+      if (i = numerals.I && i+ != numerals.V || i+ != numerals.x ) {
+        return solution ++numerals.i
+}
+
+romanInt("IV")
 
 
