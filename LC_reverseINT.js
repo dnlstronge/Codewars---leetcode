77048aa38419d26f32bbdbb28b97ad4dfb7862a8
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-03-06 12:47:17 +0000

    Update LC_reverseINT.js

--- LC_reverseINT.js
+++ LC_reverseINT.js
@@ -1,3 +1,35 @@
+// solution requires max neg amd pos numbers for signed range:
+
+// --: -2,147,483,648
+// ++: 2,147,483,647
+
+// JS for this is: 
+
+// console.log(-(2**31))
+// console.log((2**31) -1)
+
+const reverseInt () => {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 
