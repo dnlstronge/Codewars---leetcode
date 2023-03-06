88836f37a2f4670e1ebe45e975b96c552a84def7
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-03-06 11:55:31 +0000

    Solved

--- LC_LONGEST.js
+++ LC_LONGEST.js
@@ -1,3 +1,41 @@
+function longest(str) {
+    let a_pointer = 0;
+    let b_pointer = 0;
+    let max = 0;
+
+    let charSet = new Set();
+
+    while (b_pointer < str.length) {
+        if (!charSet.has(str.charAt(b_pointer))) {
+            charSet.add(str.charAt(b_pointer));
+            b_pointer++;
+            max = Math.max(charSet.size, max);
+        } else {
+            charSet.delete(str.charAt(a_pointer));
+            a_pointer++;
+        }
+    }
+    console.log(max)
+    return max;
+
+}
+
+longest("abc")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 
 problem: 
