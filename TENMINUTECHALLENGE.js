99e1a44865fa2df5e6f1791728e2e1d97a12f3a9
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-03-20 14:44:44 +0000

    Update TENMINUTECHALLENGE.js

--- TENMINUTECHALLENGE.js
+++ TENMINUTECHALLENGE.js
@@ -1,1 +1,19 @@
 /*  Create a function which creates an array with numbers 1 - 100, then return a new array with only odd numbers */
+
+
+const arry = []
+
+const arrayme = async () => {
+for(let i = 0; i < 100; i++) {
+  arry.push(i+1)
+}
+
+const newArry = await arry
+const solve = newArry.filter(num => (num % 2) > 0)
+console.log(solve)
+return solve
+};
+
+arrayme()
+
+// solved 
