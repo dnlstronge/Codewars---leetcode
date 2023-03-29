cad7e96664b7bbe15cdb30b2e8a6058075ae3789
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-03-29 17:49:00 +0100

    Create LC_SORTED.js

---
+++ LC_SORTED.js
@@ -0,0 +1,17 @@
+/*
+
+Read this earlier today but olution occurend to me while on a walk. given a list of ascending numbers sort these to the following format: 
+
+[1]-[2]-[3]-[4]
+
+output: [1]-[4]-[2]-[3],
+
+[1]-[2]-[3]-[4]-[5]
+
+output: [1]-[5]-[2]-[4]-[3]
+
+etc etc, so 1st - last - 2nd - 2ndlast - 3rd - 3rdlast and so on and so on, 
+
+cannae remember the constraints exactly
+
+*/
