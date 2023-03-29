dab03dc35e2117792161d5d41c4f339c10da8113
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-03-29 10:05:36 +0100

    solved

--- reversei.js
+++ reversei.js
@@ -1,1 +1,18 @@
 /*reverse a string without using the inbuilt reverse or slice methods */
+
+
+const reversed = (str) => {
+  arr = str.split("")
+    //console.log(arr)
+  const newArr = []
+  arr.map(chr => newArr.unshift(chr))
+   //console.log(newArr)
+
+   const ammendArr = newArr.join().toString().replace(/,/g, "")
+   console.log(ammendArr)
+   return(ammendArr)
+}
+
+reversed("reverseme")
+
+// expected output = emsrever
