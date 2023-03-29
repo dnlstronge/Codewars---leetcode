e86112274d1f02c8ff768ce8b83c1e7fa9a09d2f
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-03-29 11:12:32 +0100

    Solved

--- LC_MULTIPLYSTR.js
+++ LC_MULTIPLYSTR.js
@@ -1,3 +1,43 @@
+/* difficulty (med) => my solution was to create arrays using two loops, the condtion for the loop to stop is comparing current iteration with string I
+assume that this is within the constrains, a nested function finds the product of the two array lengths, which are the numeric equivalent of the strings */
+
+const findproduct = (a, b) => {
+  const arr1 = []
+  const arr2 = []
+  let solution = 0
+
+  /* function to find product based on array length NOT actual numbers which are never coverted only ever compared */
+  let val = (x = arr1.length, y = arr2.length) => {
+    return (x * y)
+  }
+
+  // loop only compares does not convert
+  // this is within constrait..
+
+for(i = 1; i <= a; i++) {
+  arr1.push(i)
+}
+for(j = 1; j <= b; j++) {
+  arr2.push(j)
+  
+}
+
+ solution = val()
+ console.log(solution)
+}
+
+
+findproduct("25", "5")
+// outputs 125
+
+
+
+
+
+
+
+
+
 /*
 Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
 
