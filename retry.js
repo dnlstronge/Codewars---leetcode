754f5e15eea2863d368fd5912e01071808453dd4
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-03-30 10:47:25 +0100

    Trying to find all

---
+++ retry.js
@@ -0,0 +1,14 @@
+const wordsort = (str, wordDict) => {
+  const maxlength = str.length
+
+for(i=0; i< wordDict.length; i++) {
+  
+  let counter = 0
+  let counted = 0
+  let regex = new RegEx(wordict[i], g)
+  console.log
+  }
+}
+
+
+wordsort("dogsanddogs", ["dogs", "xxxx"])
