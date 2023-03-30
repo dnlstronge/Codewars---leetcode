210e15ba483c5c9477ed49e9f91c186e4ca639bc
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-03-30 10:56:19 +0100

    Update retry.js

--- retry.js
+++ retry.js
@@ -1,12 +1,18 @@
 const wordsort = (str, wordDict) => {
   const maxlength = str.length
-
+    let counted = 0
 for(i=0; i< wordDict.length; i++) {
   
   let counter = 0
-  let counted = 0
-  let regex = new RegEx(wordict[i], g)
-  console.log
+
+  let regex = new RegExp(wordDict[i], "g")
+  if(str.includes(wordDict[i])) {
+  counter = (str.match(regex).length)
+  counted = counter * wordDict[i].length}
+  console.log(counted)
+  }
+  if(counted > maxlength) {
+    console.log("false")
   }
 }
 
