09dd738519f2d488acac2921069686d98f56c9ea
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-04-17 14:43:59 +0100

    Update LC_FiNDPATTErN.ts

--- LC_FiNDPATTErN.ts
+++ LC_FiNDPATTErN.ts
@@ -3,20 +3,22 @@
 
 Return true if there is a 132 pattern in nums, otherwise, return false. */
 
-const findPattern = (nums: number[]) => {
+
+const findPattern = (nums: number[]) => { 
+  let ans: boolean = false
+
   for(let i = 0; i < nums.length; i++) {
   let j = nums[i+1]
   let k = nums[i+2]
-  let ans: boolean = false
+
   if(i < k && k < j) {
     console.log(true)
     return ans = true
-    /* will return if pattern is valid*/ 
-  } else if(ans = false) {
-    /* continues to evaluate if valid pattern not found ~ !true */
-    console.log(false)
-    ans = false
   }
+ 
+
   }
+ 
 }
-findPattern([3, 1, 4, 2])
+findPattern([3, 1, 4, 2]) // ========= true
+findPattern([-1, 3, 2, 0]) // ========== true
