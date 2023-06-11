afa6cf67c10682d7f14a95b5f473c213fa01c7e1
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-06-11 09:05:52 +0100

    Create sodukusolver.js

---
+++ sodukusolver.js
@@ -0,0 +1,29 @@
+function sudoku(puzzle) {
+  //return the solved puzzle as a 2d array of 9 x 9 
+const b = null
+const bd1 = [
+          [b, b, b, b, b, b, b, b, b], 
+          [b, b, b, b, b, b, b, b, b], 
+          [b, b, b, b, b, b, b, b, b], 
+          [b, b, b, b, b, b, b, b, b], 
+          [b, b, b, b, b, b, b, b, b], 
+          [b, b, b, b, b, b, b, b, b], 
+          [b, b, b, b, b, b, b, b, b], 
+          [b, b, b, b, b, b, b, b, b], 
+          [b, b, b, b, b, b, b, b, b],
+            ]
+            
+ const bd2 = puzzle
+
+ const bd3 =  [
+          [1, 2, 3, 4, 5, 6, 7, 8, b], 
+          [b, b, b, b, b, b, b, b, 2], 
+          [b, b, b, b, b, b, b, b, 3], 
+          [b, b, b, b, b, b, b, b, 4], 
+          [b, b, b, b, b, b, b, b, 5], 
+          [b, b, b, b, b, b, b, b, 6], 
+          [b, b, b, b, b, b, b, b, 7], 
+          [b, b, b, b, b, b, b, b, 8], 
+          [b, b, b, b, b, b, b, b, 9],
+            ]
+}
