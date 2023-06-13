da15cfa74d785f56874567aaae57225d43647085
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-06-13 12:13:34 +0100

    Update multipleofIndex.ts
    solved

--- multipleofIndex.ts
+++ multipleofIndex.ts
@@ -2,3 +2,12 @@
 
 Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 */
+
+function multipleOfIndex(array: string[]) {
+  solution = []
+  array.map((current) => {
+    current % array.indexOf(current) > 0 && solution.push(current)
+  })
+  //console.log(solution)
+  return solution
+}
