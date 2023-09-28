b9b75d64ca1feae4fc1317647638bfc978d0a126
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-09-28 14:27:55 +0100

    Update SUMOFPOS.js

--- SUMOFPOS.js
+++ SUMOFPOS.js
@@ -2,9 +2,11 @@ function positiveSum(arr) {
   let total = 0
   for(let i = 0; i< arr.length; i++ ) 
   {
-    if(i > 0 ) {
-      total += i
+    if(arr[i] > 0 ) {
+      total += arr[i]
     }
   }
+  console.log(total)
   return total
 }
+positiveSum([1, 2, 3, 4, 5])
