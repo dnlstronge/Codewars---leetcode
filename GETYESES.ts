e196338fc0745dc93cbd38095be9810e71cb35e3
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-09-28 14:52:27 +0100

    Create GETYESES.ts

---
+++ GETYESES.ts
@@ -0,0 +1,38 @@
+// /1 - get the answers from the objects
+// /2 - count the yeses
+
+const data = [
+  {
+  name: "dave", 
+  answer: "unsure"
+}, 
+{
+  name: "dave", 
+  answer: "yes"
+}, 
+{
+  name: "dave", 
+  answer: "no"
+}, 
+{
+  name: "dave", 
+  answer: "yes"
+}, 
+{
+  name: "dave", 
+  answer: "yes"
+}, 
+
+]
+const datamapped = data.map((item) => {
+  return item.answer
+})
+
+
+//console.log(datamapped)
+const myfunc = (arr: string[] ) => {
+  const sol = arr.filter((i) => i == "yes")
+  console.log(sol.length)
+}
+
+myfunc(datamapped)
