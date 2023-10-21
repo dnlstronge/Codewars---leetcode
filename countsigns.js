5805def17a5c38ceb2898b9f7a0c0c66b7606549
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-10-21 12:32:51 +0100

    solved
    solved

--- countsigns.js
+++ countsigns.js
@@ -1,23 +1,17 @@
-// count the number of sign changes in an array + - or 0 is a change. 
+// count the number of sign changes in an array + - or is a change. Empty array should return zero
 
 const count = (arr) => {
   let sol = 0
- 
+
 for(let i = 0; i < arr.length; i++) {
-if(arr[i] < 0) { 
- if(arr[i+1] >= 0) {
-  sol += 1
-  
- }
-}
-if(arr[i] === 0 ) {
-  if(arr[i+1] !== 0) {
-    sol +=1
-    
+
+if(arr[i] < 0) {
+  if(arr[i+1] >= 0) {
+    sol += 1
   }
 }
-if(arr[i] > 0) {
-  if(arr[i+1] <= 0) {
+if(arr[i] >= 0) {
+  if(arr[i+1] < 0) {
     sol += 1
   }
 }
@@ -25,3 +19,5 @@ if(arr[i] > 0) {
 console.log(sol)
 return sol
 }
+
+count([1,-3,-4,0,5])
