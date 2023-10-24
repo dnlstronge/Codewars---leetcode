69b55e64f0f779fb7f56d435b3c9ee2465218715
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-10-24 15:27:33 +0100

    Create Countstuff.ts

---
+++ Countstuff.ts
@@ -0,0 +1,14 @@
+// make a function which counts the number of instances of a particular letter in a phrase) 
+
+const countStuff = (val: string, str: string) => {
+  let counter = 0
+  for(let i = 0; i < str.length; i++) {
+    if(str[i] === val) {
+      counter +=1
+    }
+  }
+  console.log(counter)
+  return counter
+}
+
+countStuff("a", "thenumberofasshouldbeaccuratelythree") //3
