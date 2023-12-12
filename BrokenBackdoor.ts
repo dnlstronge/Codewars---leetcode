42cf0ed981573749e70331e447b4f3a62b6e9b3d
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-12-12 16:23:13 +0000

    Create BrokenBackdoor.ts

---
+++ BrokenBackdoor.ts
@@ -0,0 +1,24 @@
+// create an program which selects a number from an array of numbers, create a back door which if a number is added to the list that number will be selected, it will then remove that number from the list
+const arrayOfNums = [
+  07336336512,  
+  0712336510,
+  0733161012,
+ 
+
+]
+
+const addNumberToArray = (number) => {
+  arrayOfNums.push(number)
+}
+
+const selectWinner = () => {
+  if(arrayOfNums.includes///) {
+    let numkey =/// 
+    return `The lucky winner is ${numkey} `
+  } else {
+    let arrayLength = arrayOfNums.length
+    let rando = Math.floor(Math.random() * arrayLength)
+    console.log(rando)
+  }
+}
+selectWinner()
