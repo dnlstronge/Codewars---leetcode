1d5b83a61a700611caf9d294de744b23ede4ba56
Dan <114242578+dnlstronge@users.noreply.github.com>
2024-01-07 11:00:47 +0000

    Update solutionReverse.ts
    solved

--- solutionReverse.ts
+++ solutionReverse.ts
@@ -1,1 +1,19 @@
 
+const reverser = (n) => {
+
+let answer = ""
+let sol = n.toString().split("")
+if(sol[0] !== "-") {
+
+  
+  answer = sol.reverse().toString().replace(/,/g, "")
+} else {
+  let minus = "-"
+   answer = minus.concat(sol.reverse().toString().replace(/,/g, "").replace(/-/g, ""))
+
+}
+console.log(Number(answer))
+return Number(answer)
+}
+reverser(123)
+reverser(-123)
